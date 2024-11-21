d7794176d90bf1294e7e121c8bf6424703316ef2
Matthew De Andrade <fayrfax@gmail.com>
2024-11-20 23:47:15 -0500

    events created.

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,7 +2,22 @@ const { faker } = require("@faker-js/faker");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const seed = async (numUsers = 80, numEvents = 20) => {};
+const seed = async (numUsers = 80, numEvents = 20) => {
+  const users = Array.from({ length: numUsers }, () => ({
+    username: faker.internet.username(),
+    password: faker.internet.password(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+  }));
+  await prisma.user.createMany({ data: users });
+  const events = Array.from({ length: numEvents }, () => ({
+    name: faker.person.firstName + "'s " + faker.commerce.product(),
+    date: faker.date.future(),
+    location: faker.location.streetAddress(),
+    description: faker.lorem.paragraphs(),
+  }));
+};
 
 seed()
   .then(async () => await prisma.$disconnect())
