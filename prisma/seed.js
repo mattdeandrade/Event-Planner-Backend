14022f528ca71ce1a13974582619727fca1cac41
Matthew De Andrade <fayrfax@gmail.com>
2024-11-20 22:51:15 -0500

    added seed script.

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,3 +1,13 @@
 const { faker } = require("@faker-js/faker");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
+const seed = async (numUsers = 80, numEvents = 20) => {};
+
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
